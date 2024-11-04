3f7b602f154e82e9d7dffda6d53163881124b698
Milkricee <jnr.daniil@gmail.com>
2024-11-04 23:12:46 +0100

    Fahrzeuge ändern und anzeigen aktualisiert

--- components/FahrzeugItem.vue
+++ components/FahrzeugItem.vue
@@ -5,7 +5,7 @@
       <div @click="toggleDetails" class="cursor-pointer font-semibold text-blue-700 hover:text-blue-900">
         {{ fahrzeug.marke }} {{ fahrzeug.modell }} - Kennzeichen: {{ fahrzeug.kennzeichen }}
       </div>
-      <!-- Bearbeitungssymbol mit Query-Parameter -->
+      <!-- Bearbeitungssymbol -->
       <NuxtLink :to="{ path: '/kfz/fahrzeug-bearbeiten', query: { kennzeichen: fahrzeug.kennzeichen } }" class="text-blue-500 hover:text-blue-700 ml-2">
         ✏️
       </NuxtLink>
@@ -13,17 +13,34 @@
 
     <!-- Detaillierte Fahrzeuginformationen -->
     <div v-if="showDetails" class="pl-4 mt-2 text-gray-700">
-      <p>Kilometerstand: {{ fahrzeug.kilometerstand }} km</p>
-      <p>Erstzulassung: {{ fahrzeug.erstzulassung }}</p>
-      <p>Kaufpreis: {{ fahrzeug.kaufpreis }} €</p>
-      <p>Garantie: {{ fahrzeug.garantie }}</p>
-      <p>Versicherungsnummer: {{ fahrzeug.versicherungs_nr }}</p>
+      <p v-if="fahrzeug.fahrgestell_nr">Fahrgestellnummer: {{ fahrzeug.fahrgestell_nr }}</p>
+      <p v-if="fahrzeug.typschluessel_nr">Typschlüsselnummer: {{ fahrzeug.typschluessel_nr }}</p>
+      <p v-if="fahrzeug.karosserie_typ">Karosserietyp: {{ fahrzeug.karosserie_typ }}</p>
+      <p v-if="fahrzeug.kraftstoff_typ">Kraftstofftyp: {{ fahrzeug.kraftstoff_typ }}</p>
+      <p v-if="fahrzeug.leistung_ps">Leistung (PS): {{ fahrzeug.leistung_ps }}</p>
+      <p v-if="fahrzeug.kaufpreis">Kaufpreis: {{ fahrzeug.kaufpreis }} €</p>
+      <p v-if="fahrzeug.bruttolistenpreis">Bruttolistenpreis: {{ fahrzeug.bruttolistenpreis }} €</p>
+      <p v-if="fahrzeug.kaufdatum">Kaufdatum: {{ fahrzeug.kaufdatum }}</p>
+      <p v-if="fahrzeug.erstzulassung">Erstzulassung: {{ fahrzeug.erstzulassung }}</p>
+      <p v-if="fahrzeug.breite">Breite: {{ fahrzeug.breite }} cm</p>
+      <p v-if="fahrzeug.laenge">Länge: {{ fahrzeug.laenge }} cm</p>
+      <p v-if="fahrzeug.hoehe">Höhe: {{ fahrzeug.hoehe }} cm</p>
+      <p v-if="fahrzeug.leermasse">Leermasse: {{ fahrzeug.leermasse }} kg</p>
+      <p v-if="fahrzeug.gesamtmasse">Gesamtmasse: {{ fahrzeug.gesamtmasse }} kg</p>
+      <p v-if="fahrzeug.max_zuladung">Max. Zuladung: {{ fahrzeug.max_zuladung }} kg</p>
+      <p v-if="fahrzeug.anhaengerkupplung !== null">Anhängerkupplung: {{ fahrzeug.anhaengerkupplung ? 'Ja' : 'Nein' }}</p>
+      <p v-if="fahrzeug.sitze">Sitze: {{ fahrzeug.sitze }}</p>
+      <p v-if="fahrzeug.kilometerstand">Kilometerstand: {{ fahrzeug.kilometerstand }} km</p>
+      <p v-if="fahrzeug.garantie">Garantie: {{ fahrzeug.garantie }}</p>
+      <p v-if="fahrzeug.versicherungs_nr">Versicherungsnummer: {{ fahrzeug.versicherungs_nr }}</p>
+      <p v-if="fahrzeug.schaden !== null">Schaden: {{ fahrzeug.schaden ? 'Ja' : 'Nein' }}</p>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+
 const props = defineProps({
   fahrzeug: Object
 });
@@ -34,3 +51,9 @@ function toggleDetails() {
   showDetails.value = !showDetails.value;
 }
 </script>
+
+<style scoped>
+.cursor-pointer:hover {
+  color: #4A90E2;
+}
+</style>
